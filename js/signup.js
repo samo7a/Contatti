ff5b595b9de9a4e05616d4fe5999bc647d445d61
Ahmed Elshetany <samo7a98@mail.com>
2021-01-22 12:35:34 -0500

    modified signup.js
    improved the error msgs for better user experience
    allowed some special characters(dash, single quote, and space) for the first name and the last name for names like: O'brien, El-shetany, Hai Rong
    limited the password length to 50

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,7 @@
 var url = 'http://thecontatti.com/signup.php';
-
+//var dublicateEmail = "Duplicate entry '88@ss.net' for key 'Users.u_email'";
+//var dublicateUserName = "Duplicate entry 'samo' for key 'Users.userName'";
+//var dublicatePhoneNumber = "Dublicat entry '12345667' for key 'Users.phoneNumber'";
 var firstName = "";
 var lastName = "";
 var password = "";
@@ -15,7 +17,12 @@ function signup() {
     userName = document.getElementById("userName").value;
     phoneNumber = document.getElementById("phoneNumber").value;
     email = document.getElementById("email").value;
-    document.getElementById("error").innerHTML = "";
+    document.getElementById("firstNameError").innerHTML = "";
+    document.getElementById("lastNameError").innerHTML = "";
+    document.getElementById("passwordError").innerHTML = "";
+    document.getElementById("userNameError").innerHTML = "";
+    document.getElementById("phoneNumberError").innerHTML = "";
+    document.getElementById("emailError").innerHTML = "";
     
     if (validateInput(firstName, lastName, password, userName, phoneNumber, email)) {
         var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "password" : "' + password + '", "userName" : "' + userName + '", "phoneNumber" : "' + phoneNumber + '", "email" : "' + email + '"}';
@@ -43,32 +50,62 @@ function signup() {
     }
 }
 
-function checkName(name) {
+function checkFirstName(name) {
     "use strict";
-    var nameREGEX = /^[A-Za-z]+$/;
+    var nameREGEX = /^[A-Za-z-,\s']+$/;
     if (name.length < 1) {
-        document.getElementById("error").innerHTML = "First and last names shouldn't be empty!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("firstNameError").innerHTML = "First name is required!";
+        document.getElementById("firstNameError").style.color = "red";
         return false;       
     }
     if (!nameREGEX.test(name)) {
-        document.getElementById("error").innerHTML = "Enter a valid Name!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("firstNameError").innerHTML = "Please enter a valid name!";
+        document.getElementById("firstNameError").style.color = "red";
         return false; 
+    }
+    if (name.length > 50) {
+        document.getElementById("firstNameError").innerHTML = "First Name should not exceed 50 characters!";
+        document.getElementById("firstNameError").style.color = "red";
+        return false;       
+    }
+    return true;
+}
+function checkLastName(name) {
+    "use strict";
+    var nameREGEX = /^[A-Za-z-,\s']+$/;
+    if (name.length < 1) {
+        document.getElementById("lastNameError").innerHTML = "*Last name is required!";
+        document.getElementById("lastNameError").style.color = "red";
+        return false;       
+    }
+    if (!nameREGEX.test(name)) {
+        document.getElementById("lastNameError").innerHTML = "please enter a valid name!";
+        document.getElementById("lastNameError").style.color = "red";
+        return false; 
+    }
+    if (name.length > 50) {
+        document.getElementById("lastNameError").innerHTML = "Last Name should not exceed 50 characters!";
+        document.getElementById("lastNameError").style.color = "red";
+        return false;
     }
     return true;
 }
 function checkPassword(password) {
     "use strict";
     var passwordREGEX = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        if (password.length === 0) {
+        document.getElementById("passwordError").innerHTML = "Password is required!";
+        document.getElementById("passwordError").style.color = "red";
+        return false;
+    }
     if (!passwordREGEX.test(password)) {
-        document.getElementById("error").innerHTML = "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 symbol, and 1 digit.<br>Password should be at least 8 characters long";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("passwordError").innerHTML = "Your password must be at least 8 characters long, contain at least<br>one number and have a mixture of uppercase and lowercase letters.";
+        document.getElementById("passwordError").style.color = "red";
         return false;
     }
     if (password.length > 50) {
-        document.getElementById("error").innerHTML = "Password should not exceed 50 characters!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("passwordError").innerHTML = "Password should not exceed 50 characters!";
+        document.getElementById("passwordError").style.color = "red";
         return false;
     }
     return true;
@@ -77,13 +114,13 @@ function checkUserName(userName) {
     "use strict";
     var userNameREGEX = /^[A-z]{1,}[A-z0-9]{0,}$/;
     if (!userNameREGEX.test(userName)) {
-        document.getElementById("error").innerHTML = "Please enter a valid User Name.<br>User Name should start with at least one Character!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("userNameError").innerHTML = "Please enter a valid User Name!<br>User Name should start with a letter!";
+        document.getElementById("userNameError").style.color = "red";
         return false;        
     }
     if (userName.length < 5 || userName.length > 50) {
-        document.getElementById("error").innerHTML = "Please enter a valid User Name!<br>User Name length must be between 5 and 50 Characters!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("userNameError").innerHTML = "Please enter a valid User Name!<br>User Name length must be between 5 and 50 Characters!";
+        document.getElementById("userNameError").style.color = "red";
         return false;       
     }
     return true;
@@ -95,15 +132,15 @@ function checkPhoneNumber(phoneNumber) {
         return true;
     }
     if (phoneNumber.length !== 10) {
-        document.getElementById("error").innerHTML = "Please enter a valid phone number!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("phoneNumberError").innerHTML = "Please enter a valid phone number!";
+        document.getElementById("phoneNumberError").style.color = "red";
         return false;
     }
     var i = 0;
     for (i = 0; i < 10; i += 1) {
         if (phoneNumber.charAt(i) < '0' && phoneNumber.charAt(i) > '9') {
-            document.getElementById("error").innerHTML = "Please enter a valid phone number!";
-            document.getElementById("error").style.color = "red";
+            document.getElementById("phoneError").innerHTML = "Please enter a valid phone number!";
+            document.getElementById("phoneError").style.color = "red";
             return false;
         } 
     }
@@ -114,8 +151,8 @@ function checkEmail(email) {
     "use strict";
     var emailREGEX = /^[^\s@]+@[^\s@\d]+\.[^\s@\d]+$/;
     if (email.length > 50) {
-        document.getElementById("error").innerHTML = "Email is too long!<br>Email should not exceed 50 characters!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("emailError").innerHTML = "Email is too long!<br>Email should not exceed 50 characters!";
+        document.getElementById("emailError").style.color = "red";
         return false;
     }
     if (email.length === 0) {
@@ -123,8 +160,8 @@ function checkEmail(email) {
         return true;
     }
     if (!emailREGEX.test(email)) {
-        document.getElementById("error").innerHTML = "Please enter a valid email address!";
-        document.getElementById("error").style.color = "red";
+        document.getElementById("emailError").innerHTML = "Please enter your email address in format:<br>yourname@example.com";
+        document.getElementById("emailError").style.color = "red";
         return false;
     }
     return true;
@@ -132,8 +169,8 @@ function checkEmail(email) {
 
 function validateInput(firstName, lastName, password, userName, phoneNumber, email) {
     "use strict";
-    if (!checkName(firstName)) return false;
-    if (!checkName(lastName)) return false;
+    if (!checkFirstName(firstName)) return false;
+    if (!checkLastName(lastName)) return false;
     if (!checkPassword(password)) return false;
     if (!checkUserName(userName)) return false;
     if (!checkPhoneNumber(phoneNumber)) return false;
