c0d6c64422d8406b4048590220638eb72b9496a7
Ahmed Elshetany <samo7a98@mail.com>
2021-02-02 16:37:49 -0500

    Fixed the Edit Button

--- js/crud.js
+++ js/crud.js
@@ -1,4 +1,6 @@
 let searchUrl = 'http://thecontatti.com/API/search.php';
+var EditButton = `<th class="align-left"><button class=" btn btn-light badge-pill" data-toggle="modal" data-target="#editContact"
+              style="height: 40px;">Edit</button></th>`;
 
 window.onload = function () {
     'use strict';
@@ -17,7 +19,7 @@ window.onload = function () {
                 var jsonArray = JSON.parse(request.responseText);
                 //console.log(jsonArray);
                 for (var i = 0; i < jsonArray.length; i++) {
-                    var row = `<tr><td>${jsonArray[i].c_firstName}</td><td>${jsonArray[i].c_lastName}</td><td>${jsonArray[i].c_phoneNumber}</td><td>${jsonArray[i].c_email}</td><td>${jsonArray[i].address}</td><td>${jsonArray[i].city}</td><td>${jsonArray[i].state}</td><td>${jsonArray[i].zip}</td><td><button onclick="editContact();">Edit</button><button onclick="deleteContact(${jsonArray[i].c_id})";>Delete</button></td></tr>`;
+                    var row = `<tr><td>${jsonArray[i].c_firstName}</td><td>${jsonArray[i].c_lastName}</td><td>${jsonArray[i].c_phoneNumber}</td><td>${jsonArray[i].c_email}</td><td>${jsonArray[i].address}</td><td>${jsonArray[i].city}</td><td>${jsonArray[i].state}</td><td>${jsonArray[i].zip}</td><td>${EditButton}<button onclick="deleteContact(${jsonArray[i].c_id})";>Delete</button></td></tr>`;
                     table.innerHTML += row;
                 }
                 if (jsonArray.msg === "No Contacts Found")
@@ -52,7 +54,7 @@ function search() {
                 var jsonArray = JSON.parse(request.responseText);
                 //console.log(jsonArray);
                 for (var i = 0; i < jsonArray.length; i++) {
-                    var row = `<tr><td>${jsonArray[i].c_firstName}</td><td>${jsonArray[i].c_lastName}</td><td>${jsonArray[i].c_phoneNumber}</td><td>${jsonArray[i].c_email}</td><td>${jsonArray[i].address}</td><td>${jsonArray[i].city}</td><td>${jsonArray[i].state}</td><td>${jsonArray[i].zip}</td><td><button onclick="editContact();">Edit</button><button "onclick="deleteContact();">Delete</button></td></tr>`;
+                    var row = `<tr><td>${jsonArray[i].c_firstName}</td><td>${jsonArray[i].c_lastName}</td><td>${jsonArray[i].c_phoneNumber}</td><td>${jsonArray[i].c_email}</td><td>${jsonArray[i].address}</td><td>${jsonArray[i].city}</td><td>${jsonArray[i].state}</td><td>${jsonArray[i].zip}</td><td>${EditButton}<button "onclick="deleteContact();">Delete</button></td></tr>`;
                     table.innerHTML += row;
                 }
                 if (jsonArray.msg === "No Contacts Found")
